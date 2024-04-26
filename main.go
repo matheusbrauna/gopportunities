b4b24d1be88364039eaa906216326e00de7b34f3
Matheus Braúna <mattsilvahenrique102@gmail.com>
2024-04-26 18:18:45 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"github.com/matheusbrauna/gopportunities/config"
 	"github.com/matheusbrauna/gopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
--- router/router.go
+++ router/router.go
@@ -3,9 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	// Initialize Router
 	router := gin.Default()
 
+	// Initialize Routes
 	initializeRoutes(router)
 
+	// Run the server
 	router.Run(":8080")
 }
