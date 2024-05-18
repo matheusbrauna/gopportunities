9f44c12a18aadcf512703de275896b8859f32c75
Matheus Braúna <matheusbrauna.contato@gmail.com>
2024-05-18 15:04:57 -0300

    docs: add documentation for show opening handler

--- handler/response.go
+++ handler/response.go
@@ -37,3 +37,8 @@ type DeleteOpeningResponse struct {
 	Message string                  `json:"message"`
 	Data    schemas.OpeningResponse `json:"data"`
 }
+
+type ShowOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
